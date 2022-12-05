c8e597e03db65fcd6dc33379a9634a22b2fe64a1
Warelan <warelan@Warelans-Mac-mini.local>
2022-12-05 02:25:39 -0600

    v1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create] 
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to root_path, notice: "SUCCESS"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  
+end
